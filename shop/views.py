ecca19cf7ab0e1cf59822eb8c562277f7cb031af
alliance <allytshindayi@gmail.com>
2023-11-16 12:10:52 +0100

    ajout des fonctionalités de la page commande

--- shop/views.py
+++ shop/views.py
@@ -10,6 +10,8 @@ def shop(request):
     }
     return render(request, 'shop/index.html', context)
 
+
+# Vue de la Page de panier de produit
 def panier(request):
 
     if request.user.is_authenticated:
@@ -31,6 +33,22 @@ def panier(request):
     return render(request, 'shop/panier.html', context)
 
 
+# Vue de la page de commande de produits
 def commande(request):
-    context = {}
+    if request.user.is_authenticated:
+        client = request.user.client
+        commande, created = Commande.objects.get_or_create(client=client, complete=False)
+        articles = commande.commandearticle_set.all()
+
+    else:
+        articles = []
+        commande = {
+            'get_panier_total':0,
+            'get_panier_article':0
+        }
+
+    context = {
+        'articles':articles,
+        'commande':commande
+    }
     return render(request, 'shop/commande.html', context)
