5124a0b0c6426be55e6a454025bb015ed85ad06b
alliance <allytshindayi@gmail.com>
2023-11-19 14:23:01 +0100

    ajout

--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 import json
+
+# les importations de mes fichiers
+from utile import panier_cookie
 
 
 # vue principale Page d'accueil Affichage des produits
@@ -14,17 +18,16 @@ def shop(request, *args, **kwargs):
         nombre_article = commande.get_panier_article
 
     else:
-        articles = []
-        commande = {
-            'get_panier_total':0,
-            'get_panier_article':0
-        }
-        nombre_article = commande['get_panier_article']
+        
+        cookie_panier = panier_cookie(request)
+        articles = cookie_panier['article']
+        commande = cookie_panier['commande']
+        nombre_article = cookie_panier['nombre_article']
 
     context = {
-        
-        'nombre_article': nombre_article,
-        'produits':produits
+
+        'produits':produits,
+        'nombre_article': nombre_article
     }
     
     return render(request, 'shop/index.html', context)
@@ -40,17 +43,15 @@ def panier(request, *args, **kwargs):
         nombre_article = commande.get_panier_article
 
     else:
-        articles = []
-        commande = {
-            'get_panier_total':0,
-            'get_panier_article':0
-        }
-        nombre_article = commande['get_panier_article']
+        cookie_panier = panier_cookie(request)
+        articles = cookie_panier['article']
+        commande = cookie_panier['commande']
+        nombre_article = cookie_panier['nombre_article']
 
     context = {
         'articles':articles,
         'commande':commande,
-        'nombre_article':nombre_article
+        'nombre_article': nombre_article
     }
     return render(request, 'shop/panier.html', context)
 
@@ -64,13 +65,11 @@ def commande(request, *args, **kwargs):
         nombre_article = commande.get_panier_article
 
     else:
-        articles = []
-        commande = {
-            'get_panier_total':0,
-            'get_panier_article':0
-        }
-        nombre_article = commande['get_panier_article']
-
+        cookie_panier = panier_cookie(request)
+        articles = cookie_panier['article']
+        commande = cookie_panier['commande']
+        nombre_article = cookie_panier['nombre_article']
+        
     context = {
         'articles':articles,
         'commande':commande,
@@ -86,7 +85,7 @@ def update_article(request, *args, **kwargs):
     data       = json.loads(request.body)
     produit_id = data['produit_id']
     action     = data['action']
-    #print(action, produit_id)
+    
 
     produit = Produit.objects.get(id=produit_id)
     client = request. user.client 
@@ -113,5 +112,27 @@ def update_article(request, *args, **kwargs):
 def traitementCommande(request, *args, **kwargs):
 
     data = json.loads(request.body)
-    print(data)
+    transation_id = datetime.now().timetuple()
+    
+    commande.transation_id = transation_id
+    if request.user.is_authenticated:
+        client = request.user.client
+        commande, created = Commande.objects.get_or_create(client=client, complete=False)
+        total = float(data['form']['total'])
+        if commande.get_panier_total:
+            commande.complete = True
+        commande.save()
+
+        if commande.produit_physique:
+            AddressChipping.objects.create(
+                client=client,
+                commande=commande,
+                adresse=data['shipping']['address'],
+                ville=data['shipping']['city'],
+                zipcode=data['shipping']['zipcode']
+            )
+    else:
+      
+        print('commande effectuer avec succès !')
+
     return JsonResponse("Votre paiement a été effectué avec succès, vous recevrez votre commande dans un instant !", safe=False)
